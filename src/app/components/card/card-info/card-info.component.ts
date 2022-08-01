b7b738e10f7f892c5aa8692f06b0e7e897d38317
Franco_Osores <franco_osores@hotmail.com>
2022-08-01 10:36:56 -0300

    Feat: reusable card component with content projection for Angular

--- src/app/components/card/card-info/card-info.component.ts
+++ src/app/components/card/card-info/card-info.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-info',
   templateUrl: './card-info.component.html',
-  styleUrls: ['./card-info.component.scss']
+  styleUrls: ['./card-info.component.scss'],
 })
 export class CardInfoComponent implements OnInit {
+  @Input() _title: string = '';
+  @Input() _price: string = '';
+  @Input() _discount: string = '';
+  @Input() _priceBefore: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
